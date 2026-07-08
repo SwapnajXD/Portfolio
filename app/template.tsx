4c31a810a87fb299625c4a3242bbb8360aab344d
SwapnajXD <swapnaj0806@gmail.com>
2026-07-08 23:44:39 +0530

    Fucked up gonna reset

--- app/template.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import { MetaverseTransition } from "@/components/motion/MetaverseTransition";
-
-export default function Template({ children }: { children: ReactNode }) {
-  return <MetaverseTransition>{children}</MetaverseTransition>;
-}
